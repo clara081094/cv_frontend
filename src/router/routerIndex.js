a86b72677dd0cca45ea7670a31a4b4df0a923b78
jean <eparis@atlassian.com>
2022-02-23 11:22:27 -0500

    refactor: remove commnets in routerIndex

--- src/router/routerIndex.js
+++ src/router/routerIndex.js
@@ -6,38 +6,6 @@ import Projects from "../pages/projects/projectsIndex.vue";
 import Resume from "../pages/resume/resumeIndex.vue";
 
 const routes = [
-  // {
-  //   path: "/",
-  //   name: "auth",
-  //   component: Auth,
-  //   redirect: "/ingresar",
-  //   children: [
-  //     {
-  //       path: "/ingresar",
-  //       name: "login",
-  //       component: Login,
-  //       meta: { requiresAuth: false },
-  //     },
-  //     {
-  //       path: "/registrar",
-  //       name: "signup",
-  //       component: Signup,
-  //       meta: { requiresAuth: false },
-  //     },
-  //     {
-  //       path: "/solicitar-cambiar-contrasena",
-  //       name: "requestPassword",
-  //       component: RequestPassword,
-  //       meta: { requiresAuth: false },
-  //     },
-  //     {
-  //       path: "/cambiar-contrasena",
-  //       name: "changePassword",
-  //       component: ChangePassword,
-  //       meta: { requiresAuth: false },
-  //     },
-  //   ],
-  // },
   {
     path: "/",
     name: "layout",
